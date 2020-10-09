f94e56060718104c2bd130613c33ccff3ed702f7
Sam Lalli <2sam.lalli@gmail.com>
2020-10-09 09:27:23 -0600

    initial commit, dependencies installed, store and reducer created

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,17 @@
+
+
+const initialState = {
+    smurf: [],
+    isLoading: false,
+    error: ''
+}
+
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+
+        
+        default:
+            return state;
+    } 
+}
